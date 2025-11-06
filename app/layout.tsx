c10c8fb0e72d7b60ee432b0c821bc1ddddd32836
sham <sham9394@gmail.com>
2025-11-06 15:46:28 +0530

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '../styles/globals.css'
 import React from 'react'
 export const metadata = { title: 'CityAssist', description: 'Citizen Assistant PWA' }
 export default function RootLayout({ children }: { children: React.ReactNode }) {
